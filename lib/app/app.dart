339feee05c0578167b3ac8f1f8a9eb5ad230f386
amit2303 <amitmishra0323@gmail.com>
2024-05-13 14:46:44 +0530

    21. Adding timer th splash screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -24,6 +24,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: RouteGenerator.getRoute,
       initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:advance_flutter/presentation/resources/assets_manager.dart';
 import 'package:advance_flutter/presentation/resources/color_manager.dart';
+import 'package:advance_flutter/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -10,11 +14,36 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+
+  Timer? _timer;
+
+  _startDelay(){
+    _timer=Timer(Duration(seconds: 2),_goNext);
+
+  }
+
+  void _goNext(){
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
   @override
+  void initState() {
+    _timer?.cancel();
+    // TODO: implement initState
+    super.initState();
+    _startDelay();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+  }
   Widget build(BuildContext context) {
     return Scaffold(
+        
         backgroundColor: ColorManager.primary,
         body: Center(child: Image(image: AssetImage(ImageAssets.splashLogo))),
     );
   }
-}r
+}
