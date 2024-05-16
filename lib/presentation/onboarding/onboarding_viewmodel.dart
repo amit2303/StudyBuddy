1b2f6b9329ebfd63eaca9f5bfef841e2e092bdd9
amit2303 <amitmishra0323@gmail.com>
2024-05-16 14:41:19 +0530

    38. OnBoarding ViewModel Logic Implementation

--- lib/presentation/onboarding/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/onboarding_viewmodel.dart
@@ -1,19 +1,27 @@
 import 'dart:async';
 import 'package:advance_flutter/domain/model.dart';
 import 'package:advance_flutter/presentation/base/baseviewmodel.dart';
+import 'package:advance_flutter/presentation/resources/assets_manager.dart';
+import 'package:advance_flutter/presentation/resources/strings_manager.dart';
 
 class OnBoardingViewModel implements BaseViewModel, OnBoardingViewModelInputs, OnBoardingViewModelOutputs{
 //stream controller
 final StreamController _streamController =StreamController<SliderViewObject>();
+
+  late final List<SliderObject> _list ;
+  int _currentIndex = 0;
   //inputs
   @override
   void dispose() {
-    // TODO: implement dispose
+    _streamController.close(); //after finishing the use of stream we need to dispose it
+                              //so our view will be disposed
   }
   
   @override
   void start() {
-    // TODO: implement start
+    _list =_getSliderData();
+    //send this slider data to our view
+    _postDataToView();
   }
   
   @override
@@ -30,8 +38,30 @@ final StreamController _streamController =StreamController<SliderViewObject>();
   void onPageChanged(int index) {
     // TODO: implement onPageChanged
   }
+  
+  @override
+  // TODO: implement inputSliderViewObject
+  Sink get inputSliderViewObject => _streamController.sink;
+  
+  //output
+  @override
+  // TODO: implement outputSliderViewObject
+  Stream<SliderViewObject> get outputSliderViewObject => _streamController.stream.map((SliderObject) => SliderObject);
 
+    List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onBoardingTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onBoardingTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onBoardingTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onBoardingTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4)
+      ];
 
+  _postDataToView(){
+    inputSliderViewObject.add(SliderViewObject(_list[_currentIndex], _list.length, _currentIndex));
+  }
 }
 
 
